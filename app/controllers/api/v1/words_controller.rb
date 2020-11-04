f1b04cf59573d82cbf9800966260419d0c088c22
Mami <nozaru.yeah@gmail.com>
2020-11-04 19:44:20 +0900

    scope作成

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -2,10 +2,10 @@ module Api
   module V1
     class WordsController < ApplicationController
       def index
-        @word = Word.where("word LIKE ?", "#{params[:word]}%")
-                    .where("word_count = ?", params[:word_count])
-                    .sample(1)
-        render json: { status: 'SUCCESS', message: 'Loaded posts', data: @word }
+        word = Word.same_first_charactor(params[:word])
+                   .same_word_count(params[:word_count])
+                   .sample(1)
+        render json: { status: 'SUCCESS', message: 'Loaded word', data: word }
       end
     end
   end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,4 @@
 class Word < ApplicationRecord
+  scope :same_first_charactor, -> ( word ) { where("word LIKE ?", "#{ word }%") }
+  scope :same_word_count, -> ( word_count ) { where("word_count = ?", word_count) }
 end
